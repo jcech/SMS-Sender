a713a2cf6b39a2645d29de35a75b0b2fedc26c3d
John Cech & Matt Cox <student@epicodus.com>
2014-04-22 15:49:40 -0700

    can send text to multiple numbers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,16 +14,25 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
 
-    if @message.text_it
-      @message.save
-      redirect_to message_path(@message)
-    else
-      redirect_to message_path(@message)
+    params[:message][:number].each do |key, number|
+      curr_contact = Contact.find_by(number: number)
+
+      if !curr_contact.number.empty?
+        @message.recipients << Recipient.create(contact_id: curr_contact.id)
+      else
+        @message.recipients << Recipient.create(
+                               contact_id: Contact.create(name: "unset", number: number).id)
+      end
     end
+
+    @message.text_it
+
+    redirect_to message_path(@message)
   end
 
 private
   def message_params
-    params.require(:message).permit(:body, :number, :user_id)
+    # params.permit(:body, :number, :user_id)
+    {body: params[:message][:body], user_id: params[:message][:user_id]}
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,19 +2,23 @@ class Message < ActiveRecord::Base
   belongs_to :user
   has_many :recipients
   has_many :contacts, through: :recipients
+  accepts_nested_attributes_for :recipients
 
   def text_it
-    response = RestClient::Request.new(
-      :method => :post,
-      :url => 'https://api.twilio.com/2010-04-01/Accounts/ACe14a24973eb88a9c1d231657a2550d0b/Messages.json',
-      :user => 'ACe14a24973eb88a9c1d231657a2550d0b',
-      :password => 'd463b5113424c2885a5803f21e1662cd',
-      :payload => { :Body => self.body,
-                    :To => self.number,
-                    :From => "3212148535"}
-    ).execute
+    recipients.each do |recipient|
 
-  rescue Exception => e
-    false
+      response = RestClient::Request.new(
+        :method => :post,
+        :url => 'https://api.twilio.com/2010-04-01/Accounts/ACe14a24973eb88a9c1d231657a2550d0b/Messages.json',
+        :user => 'ACe14a24973eb88a9c1d231657a2550d0b',
+        :password => 'd463b5113424c2885a5803f21e1662cd',
+        :payload => { :Body => body,
+                      :To => Contact.find(recipient.contact_id).number,
+                      :From => "3212148535"}
+      ).execute
+
+    end
+
+    self.save
   end
 end
